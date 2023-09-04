730cb2f91aa1a12d905d81552ca91575ac0a405b
Madhumitha0112 <143775103+Madhumitha0112@users.noreply.github.com>
2023-09-04 10:54:12 +0530

    Create Number Classifier.py
    Given an integer input, The objective is to write a code to Check if a Number is Positive or Negative.
    
    For Instance,
    Input : num = 20
    Output : The number is Positive.

---
+++ Number Classifier.py
@@ -0,0 +1,7 @@
+a=int(input())
+if (a>0):
+    print("The Number is Positive")
+elif (a==0):
+        print("The Number is Neither Positive nor Negative")
+else:
+    print("The Number is Negative")
