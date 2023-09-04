7b81c83e72574294e2c636eb3e9a34878e6f4a71
Madhumitha0112 <143775103+Madhumitha0112@users.noreply.github.com>
2023-09-04 11:43:31 +0530

    Sum_of_First_N_Natural_numbers.py

--- Sum_of_First_N_Natural_numbers.py
+++ Sum_of_First_N_Natural_numbers.py
@@ -1,7 +1,7 @@
 sum = 0
-a = int(input())
+a = int(input("Enter the Number:"))
 
 for i in range(1, a + 1):
     sum = sum + i
 
-print("The sum of the first", a, "natural numbers is:", sum)
+print("The sum of the first", a, "Natural numbers is:", sum)
